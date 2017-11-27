6529fa1dc129bf45a167d4e9edd4d0300b740c98
Pedro Chiesa <p_chiesa@hotmail.com>
2017-11-27 00:57:41 -0200

    First commit

---
+++ BasicApp/BasicAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BasicApp
+{
+    public partial class BasicAppPage : ContentPage
+    {
+        public BasicAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
