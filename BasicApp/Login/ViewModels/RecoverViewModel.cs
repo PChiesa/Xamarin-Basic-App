38fb172fec1742ae8dd190883af0bb09709cd0f9
Pedro Chiesa <pedro@ascsolutions.com.br>
2018-01-10 13:26:41 -0200

    823 - Add message informing user about password recovery

--- BasicApp/Login/ViewModels/RecoverViewModel.cs
+++ BasicApp/Login/ViewModels/RecoverViewModel.cs
@@ -8,6 +8,7 @@ using Prism.Navigation;
 using BasicApp.Login.Services;
 using System.Windows.Input;
 using Prism.Services;
+using Xamarin.Forms;
 
 namespace BasicApp.Login.ViewModels
 {
@@ -17,11 +18,13 @@ namespace BasicApp.Login.ViewModels
 
         public string Email { get; set; }
         public ICommand RecoverCommand { get; private set; }
+        public ICommand GoToSiteCommand { get; private set; }
 
         public RecoverViewModel(ILoginService loginService, INavigationService navigationService, IUIServices uiServices) : base(uiServices, navigationService)
         {
             _loginService = loginService;
             RecoverCommand = new DelegateCommand(RecoverCommandAction);
+            GoToSiteCommand = new DelegateCommand(() => Device.OpenUri(new Uri("https://www.voucherseguro.com/voucherseguro/customer/account/login/")));
         }
 
         private async void RecoverCommandAction()
